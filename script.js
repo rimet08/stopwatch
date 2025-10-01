1fd60b362136ca01d861518a660e9279e9d01bf6
rimet08 <rmetzner@proton.me>
2025-10-01 19:22:07 +0200

    Script - if

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const display = document.getElementById("display");
 let timer = null;
 let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval (updateTime, 10);
+        isRunning = true;
+    }
+}

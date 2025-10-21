1f87f3fcdca0a9a0a2c4d66f56607e54bdf936ec
rimet08 <rmetzner@proton.me>
2025-10-21 20:32:07 +0200

    padStart

--- script.js
+++ script.js
@@ -33,8 +33,13 @@ function updateTime(){{
 
     let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
     let minutes = Math.floor(elapsedTime / (1000 * 60)) % 60;
-    let second = Math.floor(elapsedTime / 1000) % 60;
+    let second = Math.floor(elapsedTime / 1000 % 60);
     let milliseconds = Math.floor (elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    second = String(second).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
 
     display.textContent = `${hours}:${minutes}:${second}:${milliseconds}`;
 
